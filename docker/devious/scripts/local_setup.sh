17f8237bebe5fa6788b3248c1f8a95a1984ac6a2
Sydney Andrade <syddevious@gmail.com>
2022-04-03 13:12:20 -0500

    /wait failing

--- docker/devious/scripts/local_setup.sh
+++ docker/devious/scripts/local_setup.sh
@@ -4,6 +4,9 @@ then
     rm /srv/circus_local.ini
 
     git fetch --all
+
+    wget https://github.com/ufoscout/docker-compose-wait/releases/download/2.9.0/wait -O /wait
+    chmod +x /wait
 else
     echo "Skipping local setup"
 fi
